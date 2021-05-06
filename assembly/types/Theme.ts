edcae0af8d2a487eba8f5c3000793360245add4f
marceline-cramer <cramermarceline@gmail.com>
2021-05-06 05:34:00 -0600

    Update Theme primary color

--- assembly/types/Theme.ts
+++ assembly/types/Theme.ts
@@ -2,6 +2,6 @@ import Color from "./Color";
 
 export default class Theme {
   static readonly white: Color = new Color(1.0, 1.0, 1.0, 1.0);
-  static readonly primary: Color = new Color(0.918, 0.648, 0.004, 1.0);
+  static readonly primary: Color = new Color(1.0, 0.45, 0.0, 1.0);
   static readonly secondary: Color = new Color(0.371, 0.371, 0.371, 1.0);
 }
