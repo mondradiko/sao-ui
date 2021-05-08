3fea64ce37c1383de7ee33342f5c97838d6fc6d7
Alex Hobson <aho113@uclive.ac.nz>
2021-05-08 15:27:08 +1200

    Use new Animation framework for button y

--- assembly/elements/RoundButton.ts
+++ assembly/elements/RoundButton.ts
@@ -3,6 +3,9 @@ import Element from "../types/Element";
 import DynamicElement from "../types/DynamicElement";
 import Theme from "../types/Theme";
 import UiPanel from "../../codegen/ui/UiPanel";
+import ColorAnimation from "../types/ColorAnimation";
+import Animation from "../types/Animation";
+import { AnimationTimingFunction } from "../types/Animation";
 
 /**
    * 0 = Entering
@@ -24,77 +27,42 @@ export default class RoundButton extends Element implements DynamicElement {
 
   is_visible: bool = false;
 
-  playing_animate_in: bool = false;
-  animate_in_length: f64 = 0.6;
   public animate_in_distance: f64 = 0.25;
+  animate_in_length: f64 = 0.6;
 
-  
-  private anim_previous_ring_color: Color = Theme.white
-  private anim_previous_circle_color: Color = Theme.white
-  private anim_previous_icon_color: Color = Theme.white
   private anim_button_status: i8 = 0;
-  private anim_button_time: f64 = 0;
   anim_change_duration: f64 = 0.15;
-  anim_ring_color: Color = Theme.white
-  anim_circle_color: Color = Theme.white
-  anim_icon_color: Color = Theme.white
+  anim_ring_color: ColorAnimation = new ColorAnimation(Theme.white, Theme.white, 0, AnimationTimingFunction.LINEAR);
+  anim_circle_color: ColorAnimation = new ColorAnimation(Theme.white, Theme.white, 0, AnimationTimingFunction.LINEAR);
+  anim_icon_color: ColorAnimation = new ColorAnimation(Theme.white, Theme.white, 0, AnimationTimingFunction.LINEAR);
+
+  anim_y: Animation = new Animation(0, 0, 0, AnimationTimingFunction.LINEAR);
 
   constructor(panel: UiPanel,
               public x: f64, public y: f64,
               public radius: f64) {
     super(panel);
+    
   }
 
   update(dt: f64): bool {
     this.fade_step += dt;
 
-    this.recalculateColor(dt);
+    this.anim_circle_color.update(dt);
+    this.anim_ring_color.update(dt);
+    this.anim_icon_color.update(dt);
+    let draw_y = this.anim_y.update(dt);
 
     if (!this.is_visible) return true;
 
-    if (this.playing_animate_in) {
-      this.drawAnimateIn()
-    } else if (this.playing_clicked_animation) {
+    if (this.playing_clicked_animation) {
       this.drawClicked()
     } else {
-      this.drawCircle(this.x, this.y, this.radius, this.anim_circle_color);
-      this.drawCircleOutline(this.x, this.y, this.radius + 0.005, 0.001, this.anim_ring_color);
+      this.drawCircle(this.x, draw_y, this.radius, this.anim_circle_color.getCurrentColor());
+      this.drawCircleOutline(this.x, draw_y, this.radius + 0.005, 0.001, this.anim_ring_color.getCurrentColor());
     }
 
     return true;
-  }
-
-  private recalculateColor(dt: f64): void {
-    let is_animating: bool = this.anim_button_time < this.anim_change_duration;
-    this.anim_button_time += dt;
-    if (is_animating) {
-      let d = this.anim_button_time / this.anim_change_duration;
-      let target_ring_color = RING_COLORS[this.anim_button_status];
-      let target_circle_color = CIRCLE_COLORS[this.anim_button_status];
-      let target_icon_color = ICON_COLORS[this.anim_button_status];
-      if (d >= 1) {
-        this.anim_ring_color = target_ring_color;
-        this.anim_circle_color = target_circle_color;
-        this.anim_icon_color = target_icon_color;
-      } else {
-        this.anim_ring_color = Color.lerp(this.anim_previous_ring_color, target_ring_color, d);
-        this.anim_circle_color = Color.lerp(this.anim_previous_circle_color, target_circle_color, d);
-        this.anim_icon_color = Color.lerp(this.anim_previous_icon_color, target_icon_color, d);
-      }
-    }
-  }
-
-  drawAnimateIn(): void {
-    let d = this.fade_step / this.animate_in_length; //from 0.0-1.0
-    let color = Theme.white;
-    if (d >= 1) {
-      d = 1;
-      this.playing_animate_in = false;
-    }
-
-    let ease_func = this.easeOutExpo(d);
-    this.drawCircle(this.x, this.y + this.animate_in_distance * (1 - ease_func), this.radius, this.anim_circle_color);
-    this.drawCircleOutline(this.x, this.y + this.animate_in_distance * (1 - ease_func), this.radius + 0.005, 0.001, this.anim_ring_color);
   }
 
   /**
@@ -105,7 +73,14 @@ export default class RoundButton extends Element implements DynamicElement {
    */
   setVisualStatus(status: i8): void {
     this.anim_button_status = status;
-    this.anim_button_time = 0;
+
+    let currentCircleColor: Color = this.anim_circle_color.getCurrentColor();
+    let currentRingColor: Color = this.anim_circle_color.getCurrentColor();
+    let currentIconColor: Color = this.anim_circle_color.getCurrentColor();
+
+    this.anim_circle_color = new ColorAnimation(currentCircleColor, CIRCLE_COLORS[status], this.anim_change_duration, AnimationTimingFunction.LINEAR);
+    this.anim_ring_color = new ColorAnimation(currentRingColor, RING_COLORS[status], this.anim_change_duration, AnimationTimingFunction.LINEAR);
+    this.anim_icon_color = new ColorAnimation(currentIconColor, ICON_COLORS[status], this.anim_change_duration, AnimationTimingFunction.LINEAR);
   }
 
   easeOutExpo(x: number): number {
@@ -130,10 +105,10 @@ export default class RoundButton extends Element implements DynamicElement {
     } else {
       d_ring = Math.pow(d_ring, 5);
     }
-    let color_circle = Color.lerp(this.anim_circle_color, Theme.white, d);
+    let color_circle = Color.lerp(this.anim_circle_color.getCurrentColor(), Theme.white, d);
     let target_ring_color = Theme.white.clone();
     target_ring_color.a = 1 - Math.sqrt(d_ring);
-    let color_ring = Color.lerp(this.anim_ring_color, target_ring_color, d);
+    let color_ring = Color.lerp(this.anim_ring_color.getCurrentColor(), target_ring_color, d);
 
     let shrink = (d - d * d) * 4.0;
     let shrink_ring = (d_ring - d_ring * d_ring) * 4.0;
@@ -174,8 +149,7 @@ export default class RoundButton extends Element implements DynamicElement {
 
   animateIn(): void {
     this.is_visible = true;
-    this.playing_animate_in = true;
+    this.anim_y = new Animation(this.y + this.animate_in_distance, this.y, this.animate_in_length, AnimationTimingFunction.EASE_OUT);
     this.fade_step = 0;
-    //TODO
   }
 }
