5d58abe2722ae059ab704c92f7f00e2ce0cb29e3
Alex Hobson <aho113@uclive.ac.nz>
2021-05-10 22:04:24 +1200

    Update color scheme to use latest color-space

--- assembly/types/Theme.ts
+++ assembly/types/Theme.ts
@@ -3,7 +3,7 @@ import Color from "./Color";
 export default class Theme {
   static readonly transparent: Color = new Color(1, 1, 1, 0);
   static readonly white: Color = new Color(1.0, 1.0, 1.0, 1.0);
-  static readonly primary: Color = new Color(1.0, 0.45, 0.0, 1.0);
+  static readonly primary: Color = new Color(0.922, 0.651, 0.004, 1.0);
   static readonly secondary: Color = new Color(0.371, 0.371, 0.371, 1.0);
   static readonly disabled_button: Color = new Color(1, 1, 1, 0.3);
 
