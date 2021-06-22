1c9e2099ed400519428c8a9698a4f3999596a0ab
marceline-cramer <cramermarceline@gmail.com>
2021-06-22 09:31:21 -0600

    Add initial UiPanel.d.ts

---
+++ assembly/types/UiPanel.d.ts
@@ -0,0 +1,19 @@
+@unmanaged declare class UiPanel {
+	@external("", "UiPanel_getWidth")
+	getWidth(): f32
+
+	@external("", "UiPanel_getHeight")
+	getHeight(): f32
+
+	@external("", "UiPanel_setSize")
+	setSize(width: f32, height: f32): void
+
+	@external("", "UiPanel_setColor")
+	setColor(r: f32, g: f32, b: f32, a: f32): void
+
+	@external("", "UiPanel_drawTriangle")
+	drawTriangle(x1: f32, y1: f32, x2: f32, y2: f32, x3: f32, y3: f32, r: f32, g: f32, b: f32, a: f32)
+}
+
+export default UiPanel;
+
